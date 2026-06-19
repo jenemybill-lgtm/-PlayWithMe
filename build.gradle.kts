0eefde00fd0c2da0b51a508c976939fdcb0ce0e5
jenemybill-lgtm <jenemybill@gmail.com>
2026-06-20 02:09:25 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,7 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation:2.3.5")
     implementation("io.ktor:ktor-serialization-gson:2.3.5")
     implementation("com.google.code.gson:gson:2.10.1")
-    
-    // MongoDB Driver - Απαραίτητο για τους φίλους!
     implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.11.0")
-    
     implementation("ch.qos.logback:logback-classic:1.4.11")
 }
 
@@ -30,11 +27,5 @@ kotlin {
 }
 
 application {
-    mainClass.set("com.example.ServerKt") 
-}
-}
-tasks.getByName<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
-    archiveBaseName.set("server")
-    archiveClassifier.set("all")
-    archiveVersion.set("")
+    mainClass.set("com.example.ServerKt")
 }
